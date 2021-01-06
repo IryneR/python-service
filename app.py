ff50beea8140f61ed84bc290f577d859f2249010
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-06 14:40:21 +0200

    working python service for jire cloud plugin, which can be up in docker

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def main():
     return "Welcome!"
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
 
+
+@app.route('/hash', methods=['GET'])
+def api_hash():
+    #hash for string
+        return jsonify({'hash': hash('thedarkdog.attlasian.net')})
+
+
+app.run()
+
+# if __name__ == "__main__":
+#   app.run(host='0.0.0.0')
