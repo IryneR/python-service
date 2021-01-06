30e296af2666d9cec281a69a9fc411d224f7e0f1
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-06 14:20:52 +0200

    working environment in docker

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+@app.route("/")
+def main():
+    return "Welcome!"
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
 
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
