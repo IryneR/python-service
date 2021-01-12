411d799d17e36ed7b686985de8a190129ffa0453
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-12 13:47:22 +0200

    post method for hash function

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -8,10 +8,18 @@ def main():
     return "Welcome!"
 
 
-@app.route('/hash', methods=['GET'])
-def api_hash():
+#@app.route('/hash', methods=['GET'])
+#def api_hash():
     #hash for string
-        return jsonify({'hash': hash('thedarkdog.attlasian.net')})
+ #       return jsonify({'hash': hash('thedarkdog.attlasian.net')})
+
+@app.route('/hash', methods=['GET', 'POST'])
+def api_hash():
+    return jsonify(request.json)
+    #content = request.json
+    #print content
+#hash for issue object
+      #return jsonify({'hash': hash('thedarkdog.attlasian.net')})
 
 
 app.run()
