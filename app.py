88e5c6eadddc9c898feecb299b50767ef8ce298a
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-12 16:42:52 +0200

    working app

--- app.py
+++ app.py
@@ -8,18 +8,10 @@ def main():
     return "Welcome!"
 
 
-#@app.route('/hash', methods=['GET'])
-#def api_hash():
-    #hash for string
- #       return jsonify({'hash': hash('thedarkdog.attlasian.net')})
-
-@app.route('/hash', methods=['GET', 'POST'])
+@app.route('/hash', methods=['GET'])
 def api_hash():
-    return jsonify(request.json)
-    #content = request.json
-    #print content
-#hash for issue object
-      #return jsonify({'hash': hash('thedarkdog.attlasian.net')})
+    #hash for string
+      return jsonify({'hash': hash('thedarkdog.attlasian.net')})
 
 
 app.run()
