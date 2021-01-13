b2f004f993d92c44040388d54b68896af259b470
Irina Romanyuk <romaniuk.iv@gmail.com>
2021-01-13 10:56:35 +0200

    return hash function of object from java service

--- app.py
+++ app.py
@@ -8,24 +8,17 @@ def main():
     return "Welcome!"
 
 
-#@app.route('/hash', methods=['GET'])
-#def api_hash():
-    #hash for string
- #       return jsonify({'hash': hash('thedarkdog.attlasian.net')})
-
 @app.route('/hash', methods=['GET', 'POST'])
 def api_hash():
-    #content = request.get_json(silent=True)
-    #print content
-    #return jsonify(request.json)
-    #content = request.json
     content = request.json
-    print(content)
+    #print(content)
 
-    #res = json.loads(content)
-    #print(res)
-    #print(request.json()[0]['projectKey'])
-    return jsonify({'hash': hash('thedarkdog.attlasian.net')})
+    #use json fild concatenation
+    #keyString = content["projectKey"]+content["issueTypeName"]+content["creatorDisplayName"]+content["key"]
+    #print(keyString)
+    #return jsonify({'hash': hash(keyString)})
+
+    return jsonify({'hash': hash(str(content))})
 
 
 app.run()
